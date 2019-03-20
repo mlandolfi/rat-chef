e12eb0a5f196a5cdbe81df191ecd5ee9a636114f
ubuntu <tdferrazzano@yahoo.com>
2019-03-20 15:15:45 -0400

    added updatePopluationMean, updateStdDev, updatezScore, zScore, collectRecordedValues, removed percentChange/anything dealing with percentChange, added populationMeans,stdDevs,zScores

--- Stocks.py
+++ Stocks.py
@@ -2,7 +2,12 @@
 import json
 import datetime
 import statistics
-import pandas as pd
+
+""" notes
+One would have to divide the standard deviation by the closing price to directly compare volatility for the two securities.
+https://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:standard_deviation_volatility
+https://stattrek.com/statistics/dictionary.aspx?definition=z_score
+"""
 
 class Stock(object):
 
@@ -13,9 +18,13 @@ class Stock(object):
 		self.dataFile = dataFile
 		self.lastValueRecorded = ()
 		self.previousValues = {}	# key is date, value is value {} ^^
-		self.volumeDeviationPercentage = 0 #most recent volume compared to all previous volumes
+		self.populationMeans = () #(volatility, volume) -> means of all recorded volatilities and volumes, including today
+		self.stdDevs = () #(volatility, volume) -> numerical value used to indicate how widely individuals in a group vary. If individual observations vary greatly from the group mean, the standard deviation is big; and vice versa.
+		self.zScores = () #(volatility, volume) -> z-score indicates how many standard deviations an element is from the mean, see website in notes for more info
 		# functions to execute on instantiation
 		self.readValues()
+
+	""" ############################ Functions that work with our data files ############################ """
 
 	""" adds a time and value into self.values if it isn't already in there,
 		also sets the self.lastValueRecorded to a tuple of (time, value, volume)
@@ -23,14 +32,13 @@ class Stock(object):
 	def addValue(self, time, value, volume, day):
 		if (day == datetime.date.today().strftime('%Y-%m-%d')):	# today so add it to self.values
 			if (not time in self.values.keys()):
-				self.lastValueRecorded = (time, value, volume)
+				self.lastValueRecorded = (value, volume, time)
 				self.values[time] = (value, volume)
 		else:	# not today so previous value
 			if (not day in self.previousValues.keys()):
 				self.previousValues[day] = {}
 			if (not time in self.previousValues[day].keys()):
 				self.previousValues[day][time] = (value, volume)
-
 
 	""" combines previous values and the current days values and saves
 		them to a self.dataFile """
@@ -50,33 +58,44 @@ class Stock(object):
 			for time, value in dayValues.items():
 				self.addValue(time, float(value[0]), float(value[1]), day)
 
-	def updateVolatility(self):
-		pass
+	""" ############################ Helper functions for update functions ############################ """
 
-	"""Function to calculate the percent difference between two values, used for
-	calculating self.volumeDeviationPercentage using the stdDev of all previous volumes
-	and the latest recorded volume """
-	def getChange(self, current, previous):
-		if current == previous:
-			return 0
-		try:
-			return (abs(current - previous) / previous) * 100.0
-		except ZeroDivisionError:
-			return 0 #Not sure if this is the best way to handle division by 0, probably is
-
-	"""Calculates volumeDeviationPercentage using the std dev of all previous volumes
-	and the last recorded volume"""
-	def updateVolumeDeviationPercentage(self):
-		tempList = []
-		#going through all of today's recorded volumes
+	""" returns a list containing all recorded values for this stock if index is 0, 
+		or all recorded volumes for this stock if index is 1"""
+	def collectRecordedValues(self, index):
+		retList = []
+		#going through all of today's recorded values if index is 0 or volumes if index 1
 		for time, valueTuple in self.values.items():
-			tempList.append(valueTuple[1]) #append volume
+			retList.append(valueTuple[index]) 
 		#going through all of our past day recorded volumes
 		for day, dayValues in self.previousValues.items(): 	
 			for time, valueTuple in dayValues.items():
-				tempList.append(valueTuple[1]) #appending volume
-		stdDev = statistics.stdev(tempList) #standard deviation of all volumes (what is normal)
-		self.volumeDeviationPercentage = self.getChange(self.lastValueRecorded[2], stdDev)
+				retList.append(valueTuple[index]) 
+		return retList
+
+	"""Calculates z-score for this stock. Requires that self.populationMeans and self.stdDevs 
+		have been set and updated.
+		index is 0 if we are finding z-score of values (for volatility), 1 if z-score of volumes"""
+	def zScore(self, index):
+		#z = (X - μ) / σ, where X is latest recorded value, μ is the population mean, and σ is the standard deviation
+		return (self.lastValueRecorded[index] - self.populationMeans[index]) / self.stdDevs[index]
+
+	""" ############################ Update stock functions ############################ """
+
+	"""Updates the population mean (all recorded items, from today and past). index 0 for values, 1 for volumes
+		currenList is obtained from collectRecordedValuesFunction, which has the same index 0/1 rules."""
+	def updatePopulationMean(self, index, currentList):
+		self.populationMeans[index] = statistics.mean(currentList)
+
+	"""Updates the stdDev (all recorded items, from today and past). index 0 for values, 1 for volumes"""
+	def updateStdDev(self, index, currentList):
+		self.stdDevs[index] = statistics.stddev(currentList)
+
+	"""Requires that updatePopulationMeans/updateStdDev have both been called, in that order.
+		Updates z-score for this stock. 
+		index is 0 if we are finding z-score of values (for volatility), 1 if z-score of volumes"""
+	def updateZScore(self, index, currentList):
+		self.zScores[index] = zScore(index)
 
 	def __str__(self):
 		return self.symbol
