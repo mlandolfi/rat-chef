ebf70928f4bfe9f3729f04697d0fec53e2791008
Mike Landolfi <mlandolfi99@gmail.com>
2019-05-05 15:33:05 -0700

    deleted extra func

--- main.py
+++ main.py
@@ -9,26 +9,7 @@ from AllStocks import ALL_STOCKS
 from FileManager import FileManager
 
 # Constants
-API_KEY = "878U8SIR8IMVPVQ6"
-
-""" Updates all stock data, takes a while because we can't access the API too fast,
-	takes roughly 15 secs for each stock it's updating"""
-def updateData():
-	print("Updating %d stocks, time estimate: %d mins" % (len(ALL_STOCKS), len(ALL_STOCKS)*15/60))
-	fileManager = FileManager("../../Dropbox/")
-	ts = TimeSeries(key=API_KEY)
-	for key, stock in ALL_STOCKS.items():
-		try:
-			fileManager.loadValues(stock)
-			data, metaData = ts.get_intraday(symbol=stock.symbol, interval="5min", outputsize="full")
-			for dataKey, item in data.items():
-				stock.addValue(dataKey.split(" ")[1], item["4. close"], item["5. volume"], dataKey.split(" ")[0])
-			fileManager.storeValues(stock)
-			print ("Updated: %s" % stock.symbol)
-		except Exception as e:
-			print ("Failed to update: %s" % stock.symbol)
-		time.sleep(15)
-		
+API_KEY = "878U8SIR8IMVPVQ6"	
 		
 
 """ Updates all stock data, takes a while because we can't access the API too fast,
